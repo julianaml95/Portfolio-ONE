04a18bd817c2cd0015c1f3c90e37cd8e44d207e7
Juliana Mora López <jmora216@unicauca.edu.co>
2023-06-26 17:19:07 -0500

    CSS modifications for responsive design and button size adjustment
    This commit includes several CSS modifications to improve the responsive design of the website and adjust the size of buttons for screens with a maximum width of 768px.

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function toggleNavbarColor() {
 
 // Detectar el evento de scroll y llamar a la función
 window.addEventListener('scroll', toggleNavbarColor);
+
+// Toggle Menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
